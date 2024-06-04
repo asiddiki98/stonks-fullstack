455512c712c98d7944aa4fb88ce41ff0801a64a0
asiddiki98 <azimsardines@gmail.com>
2024-06-04 13:28:45 -0400

    removed unnecessary files

--- app/api/websockets/notify-followers/route.ts
+++ app/api/websockets/notify-followers/route.ts
@@ -59,13 +59,13 @@ export async function GET(request: Request) {
       }
 
       // send email to the offline followers
-      // console.log("offlineFollowers", offlineFollowers);
-      // if (offlineFollowers.length > 0) {
-      //   for (const follower of offlineFollowers) {
-      //     const message = `Hello ${follower.username}, ${username} has started streaming. Check it out!`;
-      //     await sendEmail(follower.email, "New Stream Alert", message);
-      //   }
-      // }
+      console.log("offlineFollowers", offlineFollowers);
+      if (offlineFollowers.length > 0) {
+        for (const follower of offlineFollowers) {
+          const message = `Hello ${follower.username}, ${username} has started streaming. Check it out!`;
+          await sendEmail(follower.email, "New Stream Alert", message);
+        }
+      }
     }
   }
   // notify followers
